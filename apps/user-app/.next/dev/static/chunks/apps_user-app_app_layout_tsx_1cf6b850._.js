1228c3c56c97f6707606fa101061a3178019318b
Akasho09 <aakashbwd@gmail.com>
2026-01-27 00:34:02 +0530

    g

--- apps/user-app/.next/dev/static/chunks/apps_user-app_app_layout_tsx_1cf6b850._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/apps_user-app_app_f4e267d9._.css"
-],
-    source: "dynamic"
-});
